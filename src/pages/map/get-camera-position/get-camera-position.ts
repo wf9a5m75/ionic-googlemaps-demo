2ecfc5b873da46b47d5dfb70e37ec605fdf37184
wf9a5m75 <wf9a5m75@gmail.com>
2017-08-20 16:36:45 -0700

    Add: map.getCameraPosition()

--- src/pages/map/get-camera-position/get-camera-position.ts
+++ src/pages/map/get-camera-position/get-camera-position.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the GetCameraPositionPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent
+} from '@ionic-native/google-maps';
 
 @IonicPage()
 @Component({
@@ -14,12 +12,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'get-camera-position.html',
 })
 export class GetCameraPositionPage {
+  map: GoogleMap;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private googleMaps: GoogleMaps, private alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad GetCameraPositionPage');
+    var self = this;
+    setTimeout(this.loadMap.bind(self), 1000);
+  }
+
+  loadMap() {
+    this.map = this.googleMaps.create("map_canvas");
+    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+
+    });
+  }
+
+  onButton_click() {
+    // Get the current camera position.
+    var cameraPosition: any = this.map.getCameraPosition();
+
+    // Show the results
+    var text = ["Current camera position:\n",
+      "-------------------------------",
+      "latitude:" + cameraPosition.target.lat,
+      "longitude:" + cameraPosition.target.lng,
+      "zoom:" + cameraPosition.zoom,
+      "tilt:" + cameraPosition.tilt,
+      "bearing:" + cameraPosition.bearing].join("<br>\n");
+
+    let alert = this.alertCtrl.create({
+      title: 'Current camera target',
+      subTitle: text,
+      buttons: ['Dismiss']
+    });
+    alert.present();
   }
 
 }
