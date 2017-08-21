3031cfe513386f638bc7ac4047a72cdabd87e0b6
wf9a5m75 <wf9a5m75@gmail.com>
2017-08-20 17:30:44 -0700

    Add: map.getCameraZoom()

--- src/pages/map/get-camera-zoom/get-camera-zoom.ts
+++ src/pages/map/get-camera-zoom/get-camera-zoom.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent
+} from '@ionic-native/google-maps';
 
-/**
- * Generated class for the GetCameraZoomPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,12 +13,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'get-camera-zoom.html',
 })
 export class GetCameraZoomPage {
+  map: GoogleMap;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private googleMaps: GoogleMaps, private alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad GetCameraZoomPage');
+    setTimeout(this.loadMap.bind(this), 1000);
+  }
+  loadMap() {
+
+    this.map = this.googleMaps.create("map_canvas");
+    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+      console.log("map is ready");
+    });
+
   }
 
+  onButton_click() {
+    var text = "zoom: " + this.map.getCameraZoom();
+
+    let alert = this.alertCtrl.create({
+      title: 'Current camera zoom level',
+      subTitle: text,
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
 }
