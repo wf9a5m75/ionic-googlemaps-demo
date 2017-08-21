fa06b73741b012a8f8246f54fd038fcd5e4f869b
wf9a5m75 <wf9a5m75@gmail.com>
2017-08-20 23:04:43 -0700

    Add: map.fromPointToLatLng()

---
+++ src/pages/map/from-point-to-lat-lng/from-point-to-lat-lng.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { FromPointToLatLngPage } from './from-point-to-lat-lng';
+
+@NgModule({
+  declarations: [
+    FromPointToLatLngPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(FromPointToLatLngPage),
+  ],
+})
+export class FromPointToLatLngPageModule {}
