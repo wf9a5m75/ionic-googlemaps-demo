e48a701fe2cc7f9d15626b9fbe3717144138238e
wf9a5m75 <wf9a5m75@gmail.com>
2017-11-08 11:35:51 -0800

    Add example for map.setPadding()

---
+++ src/pages/map/set-padding/set-padding.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SetPaddingPage } from './set-padding';
+
+@NgModule({
+  declarations: [
+    SetPaddingPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SetPaddingPage),
+  ],
+})
+export class SetPaddingPageModule {}
---
+++ src/pages/map/set-padding/set-padding.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent
+} from '@ionic-native/google-maps';
+
+@IonicPage()
+@Component({
+  selector: 'page-set-padding',
+  templateUrl: 'set-padding.html',
+})
+export class SetPaddingPage {
+  map: GoogleMap;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SetPaddingPage');
+    this.loadMap();
+  }
+
+  loadMap() {
+    this.map = GoogleMaps.create('map_canvas', {
+      'controls': {
+        'compass': true,
+        'myLocationButton': true,
+        'indoorPicker': true,
+        'zoom': true
+      },
+      'camera': {
+        'target': {
+          'lat': 41.796875,
+          'lng': 140.757007
+        },
+        'tilt': 30,
+        'zoom': 15,
+        'bearing': 50
+      }
+    });
+
+    this.map.one(GoogleMapsEvent.MAP_READY).then(() => {
+      // Usage case #1:
+      // top = right = bottom = left = 30px
+      //map.setPadding( 30 );
+
+      // Usage case #2:
+      // top = bottom = 30px
+      // left = right = 50px
+      //map.setPadding( 30, 50 );
+
+      // Usage case #3:
+      // top = 30px
+      // left = right = 50px
+      // bottom = 20px
+      //map.setPadding( 30, 50 , 20 );
+
+      // Usage case #4:
+      // top = 30px
+      // right = 50px
+      // bottom = 20px
+      // left = 10px
+      this.map.setPadding( 30, 50 , 20 , 10 );
+    });
+  }
+
+}
