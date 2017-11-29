9daf6a5da913f9353387f639cd362fa93d7f8b05
wf9a5m75 <wf9a5m75@gmail.com>
2017-11-28 16:44:11 -0800

    Add: htmlInfoWindow.setContent() example

---
+++ src/pages/html-info-window/set-content/set-content.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SetContentPage } from './set-content';
+
+@NgModule({
+  declarations: [
+    SetContentPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SetContentPage),
+  ],
+})
+export class SetContentPageModule {}
---
+++ src/pages/html-info-window/set-content/set-content.ts
@@ -0,0 +1,127 @@
+import { Platform, IonicPage } from 'ionic-angular';
+import {
+  Component,
+  ComponentRef,
+  Injector,
+  ApplicationRef,
+  ComponentFactoryResolver,
+  NgZone,
+  ElementRef,
+  ViewChild
+} from '@angular/core';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  BaseArrayClass,
+  MarkerOptions,
+  Marker,
+  HtmlInfoWindow
+} from '@ionic-native/google-maps';
+
+
+@Component({
+  selector: 'custom-tag',
+  template: `
+    <div style="white-space: nowrap;">markerTitle: {{myTitle}}</div>
+    <button ion-button (click)="onButton_click($event)">hi</button>
+  `,
+})
+export class CustomTag {
+  myTitle: string;
+
+  onButton_click(event) {
+    alert(`This is '${this.myTitle}'`);
+  }
+}
+
+
+@IonicPage()
+@Component({
+  selector: 'page-set-content',
+  templateUrl: 'set-content.html',
+})
+export class SetContentPage {
+
+  map: GoogleMap;
+  htmInfoWindow: HtmlInfoWindow;
+
+  constructor(
+    public platform: Platform,
+    private injector: Injector,
+    private resolver: ComponentFactoryResolver,
+    private appRef: ApplicationRef,
+    private _ngZone: NgZone
+  ) { }
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    let locations: MarkerOptions[] = [
+      {"myTitle": "one", disableAutoPan: true, position: {lat: 41.624615, lng: 0.6238626}},
+      {"myTitle": "two", disableAutoPan: true, position: {lat: 41.624615, lng: 0.6248626}},
+      {"myTitle": "three", disableAutoPan: true, position: {lat: 41.624615, lng: 0.6258626}}
+    ];
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: [
+          {lat: 41.624615, lng: 0.6238626},
+          {lat: 41.624615, lng: 0.6248626},
+          {lat: 41.624615, lng: 0.6258626}
+        ],
+        padding: 60,
+        tilt: 90
+      }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+    this.map.one(GoogleMapsEvent.MAP_READY)
+      .then(() => {
+        this.htmInfoWindow =  new HtmlInfoWindow();
+
+        // Create markers
+        let mvcArray: BaseArrayClass<MarkerOptions> = new BaseArrayClass<MarkerOptions>(locations);
+        mvcArray.mapAsync((markerOpts: MarkerOptions, next: (marker: Marker) => void) => {
+          this.map.addMarker(markerOpts).then(next);
+        }).then((markers: Marker[]) => {
+
+          // Listen the MARKER_CLICK event on all markers
+          markers.forEach((marker: Marker) => {
+            marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe((parmas: any[]) => {
+              this.onMarkerClick(parmas);
+            });
+          });
+        });
+
+      });
+  }
+
+  onMarkerClick(params: any[]) {
+    // Get a marker instance from the passed parameters
+    let marker: Marker = params.pop();
+
+    // Create a component
+    const compFactory = this.resolver.resolveComponentFactory(CustomTag);
+    let compRef: ComponentRef<CustomTag> = compFactory.create(this.injector);
+    compRef.instance.myTitle = marker.get("myTitle");
+    this.appRef.attachView(compRef.hostView);
+
+    let div = document.createElement('div');
+    div.appendChild(compRef.location.nativeElement);
+
+    // Dynamic rendering
+    this._ngZone.run(() => {
+      this.htmInfoWindow.setContent(div);
+      this.htmInfoWindow.open(marker);
+    });
+
+    // Destroy the component when the htmlInfoWindow is closed.
+    this.htmInfoWindow.one(GoogleMapsEvent.INFO_CLOSE).then(() => {
+      compRef.destroy();
+    });
+  }
+}
